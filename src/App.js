b78ae0eaa793e54321675b66b08250e5895f90dd
Euna Kim <kesilver46@gmai.com>
2022-12-29 18:37:29 +0900

    movie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function App() {
-  return (
-    <div>
-      <h1>Welcome back!</h1>
-    </div>
-  );
+  return <Router>
+    <Switch>
+      <Route path="/movie/:id">
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>;  
 }
 
 export default App;
